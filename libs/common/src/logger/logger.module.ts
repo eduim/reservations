883d722101baf664b6f0fe557d861a3799924b28
eduim <eduardo.im92@gmail.com>
2023-11-27 18:10:13 +0100

    common: logger

---
+++ libs/common/src/logger/logger.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { LoggerModule as PinoLogger } from 'nestjs-pino';
+@Module({
+  imports: [
+    PinoLogger.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+  ],
+})
+export class LoggerModule {}
