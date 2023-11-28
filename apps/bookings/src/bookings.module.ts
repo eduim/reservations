f0988fe08dbc72e642e2496395c43d8a8a575437
eduim <eduardo.im92@gmail.com>
2023-11-28 18:44:12 +0100

    apps/booking: env variables in microservice

--- apps/bookings/src/bookings.module.ts
+++ apps/bookings/src/bookings.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
 import { BookingsService } from './bookings.service';
 import { BookingsController } from './bookings.controller';
 import { DatabaseModule } from '@app/common';
 import { BookingRepository } from './bookings.repository';
 import { BookingDocument, BookingSchema } from './models/booking.schema';
 import { LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     DatabaseModule,
@@ -15,6 +17,13 @@ import { LoggerModule } from '@app/common';
       },
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
   ],
   controllers: [BookingsController],
   providers: [BookingsService, BookingRepository],
